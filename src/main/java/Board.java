5a661ca7a7fa35b64acf57d0d2bc99a15cf6f78a
purgz <henbrooks6@gmail.com>
2023-09-20 18:09:08 +0100

    added function to set enpassant target and convert chess square to board index

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,13 +1,14 @@
-
+import java.util.Arrays;
 
 public class Board {
 
     private static final String startingFen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
     private static final String testFen = "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2 ";
+    private static final String enPassantFen = "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq c6 0 1";
 
     //want to convert any fen position into board representation
 
-    private static final char[] squares = new char[64];
+    private final char[] squares = new char[64];
 
     private char sideToMove;
 
@@ -16,14 +17,102 @@ public class Board {
     private boolean blackKingCastle;
     private boolean blackQueenCastle;
 
-    private static void populateBoard(String fen){
+    //need to check fen for en passant target square
+    private int enPassantTargetSquare;
+
+    private void createGameBoard(String fen){
 
         String[] fenParts = fen.split(" ");
 
+        populateBoard(fenParts[0]);
+        sideToMove = fenParts[1].charAt(0);
+        setCastlingRights(fenParts[2]);
+        setEnpassantTargetSquare(fenParts[3]);
+    }
+
+    @Override
+    public String toString() {
+        return "Board{" +
+                "squares=" + Arrays.toString(squares) +
+                ", sideToMove=" + sideToMove +
+                ", whiteKingCastle=" + whiteKingCastle +
+                ", whiteQueenCastle=" + whiteQueenCastle +
+                ", blackKingCastle=" + blackKingCastle +
+                ", blackQueenCastle=" + blackQueenCastle +
+                '}';
+    }
+
+    private void setEnpassantTargetSquare(String targetSquare){
+
+        if (targetSquare.equals("-")){
+            enPassantTargetSquare = -1;
+        } else {
+            enPassantTargetSquare = convertSquarePosToBoardIndex(targetSquare);
+        }
+    }
+
+    //converts a chess position such as e1 to the correct board index
+    private int convertSquarePosToBoardIndex(String square){
+
+        char fileChar = square.charAt(0);
+        char rankChar = square.charAt(1);
+
+        // - 'a' will give correct rank corresponding to the letter a - g
+        int file = fileChar - 'a';
+
+        //- '0' will give the digit char as an int
+        // 8 - num as the board works from top to bottom
+        int rank = 8 - (rankChar - '0');
+
+        int boardIndex = (rank * 8) + file;
+
+        return boardIndex;
+    }
+
+    private void setCastlingRights(String castlingRights) {
+
+        if (castlingRights.equals("-")){
+            // '-' means neither side has castling rights
+            whiteKingCastle = false;
+            whiteQueenCastle = false;
+            blackKingCastle = false;
+            blackQueenCastle = false;
+        } else {
+
+            for (int i = 0; i < castlingRights.length(); i++){
+
+                char currentChar = castlingRights.charAt(i);
+
+                switch(currentChar)
+                {
+                    case 'K':
+                        whiteKingCastle = true;
+                        break;
+                    case 'Q':
+                        whiteQueenCastle = true;
+                        break;
+                    case 'k':
+                        blackKingCastle = true;
+                        break;
+                    case 'q':
+                        blackQueenCastle = true;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+
+    }
+
+    private void populateBoard(String pieces){
+
         int boardIndex = 0;
 
-        for (int i = 0; i < fenParts[0].length(); i++){
-            char currentChar = fenParts[0].charAt(i);
+        for (int i = 0; i < pieces.length(); i++){
+
+            char currentChar = pieces.charAt(i);
 
             if (Character.isDigit(currentChar)){
                 boardIndex += currentChar - '0';
@@ -34,16 +123,14 @@ public class Board {
                     boardIndex++;
                 }
             }
-
         }
     }
 
     public static void main(String[] args) {
 
 
-        populateBoard(startingFen);
-        System.out.println(squares);
-
-        System.out.println(squares[20] == 0);
+        Board board = new Board();
+        board.createGameBoard(enPassantFen);
+        System.out.println(board);
     }
 }
